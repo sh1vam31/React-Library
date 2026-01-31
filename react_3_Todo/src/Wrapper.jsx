93f19bcbd7097c764f86972b85cd1b6bdfcc525d
ShivamChaudhary <sc31122004@gmail.com>
2026-02-01 01:06:26 +0530

    update

--- react_3_Todo/src/Wrapper.jsx
+++ react_3_Todo/src/Wrapper.jsx
@@ -1,4 +1,5 @@
-import React, { createContext, useState } from 'react';
+import { createContext } from "react"
+import { useState } from "react"
 
 // Context  —  Prop drilling se bachne ke liye ham context ka use karte hai context matlab akk component banao orr jsme sara data daal do orr usko export kar do orr jis jis file me vo data ki need ho vaha par usko import kar do orr context jis principle par kaam karta hai usko bolte hai HOC (Higher Order Components) 
 // context universal data to hold karta hai 
@@ -6,17 +7,17 @@ export const todocontext = createContext(null);
 
 const Wrapper = (props) => {
 
-    const [todo, settodo] = useState(
-        [
-          { id: 1, title: 'Kaam karle bhai', completed: false },
-        ]
-      )
+  const [todo, settodo] = useState(
+    [
+      { id: 1, title: 'Kaam karle bhai', completed: false },
+    ]
+  )
 
   return (
     <todocontext.Provider value={[todo, settodo]}>
-         {props.children}
-    </todocontext.Provider> 
-    
+      {props.children}
+    </todocontext.Provider>
+
   )
 }
 
